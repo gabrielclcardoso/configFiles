a9424165a11a3155a230cc036a9f9a511a7a4f0a
gcorreia <gcorreia@student.42.rio>
2023-02-07 19:01:11 -0300

    created command

--- nvim/lua/plugins/Canonical-plugin/Canonical.lua
+++ nvim/lua/plugins/Canonical-plugin/Canonical.lua
@@ -4,6 +4,17 @@ function	get_extension(var)
 	return vim.api.nvim_get_var(var)
 end
 
-local ext = get_extension("extension")
+--local ext = get_extension("extension")
 
-print("File extension = <" .. ext .. ">")
+function	insertCanonicalClass()
+	local ext = get_extension("extension")
+--	print("args = " .. vim.api.args)
+	print("ext = " .. ext)
+	if (ext == ".cpp") then
+		print("source file")
+	elseif (ext == ".hpp") then
+		print("header file")
+	end
+end
+
+vim.api.nvim_create_user_command('Canonical', insertCanonicalClass, {})
