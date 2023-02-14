6c1242cff4ade33e3c341173afe9be9bfd07b903
GabrielCLC <gabrielclcardoso@gmail.com>
2023-02-14 13:58:52 -0300

    added lsp to neovim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,6 @@
 require("plugins.packer")
 require("plugins.Canonical")
 require("plugins.treesitter")
+require("plugins.lsp")
 local path = os.getenv("HOME") .. "/.config/nvim/lua/plugins/stdheader.vim"
 vim.cmd { cmd = 'source', args = { path } }
