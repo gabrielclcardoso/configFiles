25a28390fd6cb45792da9b0fbdaa4fed2beee920
GabrielCLC <gabrielclcardoso@gmail.com>
2023-03-27 16:46:28 -0300

    Feat: script now checks if programs are installed

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+alacritty --version &> /dev/null
+[[ $? -ne 0 ]] && echo "Alacritty not installed"
+nvim --version &> /dev/null
+[[ $? -ne 0 ]] && echo "nvim not installed"
+tmux -V &> /dev/null
+[[ $? -ne 0 ]] && echo "tmux not installed"
+
 cp -r alacritty $HOME/.config
 cp -r nvim $HOME/.config
 cp font/* $HOME/.local/share/fonts
