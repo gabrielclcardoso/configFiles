9063eb95241b9f7ce486c1dd1e8fca06cef76c63
GabrielCLC <gabrielclcardoso@gmail.com>
2023-04-18 18:27:01 -0300

    feat: changed some keybinds

--- nvim/lua/configs/remap.lua
+++ nvim/lua/configs/remap.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cc", vim.cmd.Canonical)
 vim.keymap.set("n", "<leader>hs", vim.cmd.split)
 vim.keymap.set("n", "<leader>vs", vim.cmd.vsplit)
-vim.keymap.set("n", "<leader>ww", vim.cmd.write)
+vim.keymap.set("n", "<leader>w", vim.cmd.write)
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -16,4 +16,4 @@ vim.keymap.set('n', '<leader>g', telescope.git_files, {})
 --nvim-tree binds--
 local api = require('nvim-tree.api')
 
-vim.keymap.set('n', '<leader>ee', api.tree.toggle, {})
+vim.keymap.set('n', '<leader>e', api.tree.toggle, {})
