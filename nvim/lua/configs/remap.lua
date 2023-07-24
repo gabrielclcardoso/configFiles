bc3acdf339e9692bb82b3a126fa1de9a6f368204
GabrielCLC <gabrielclcardoso@gmail.com>
2023-07-24 12:42:28 -0300

    feat: toggle for tab showing

--- nvim/lua/configs/remap.lua
+++ nvim/lua/configs/remap.lua
@@ -5,8 +5,18 @@ vim.keymap.set("n", "<leader>v", vim.cmd.split)
 vim.keymap.set("n", "<leader>s", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>w", vim.cmd.write)
 
+vim.keymap.set("n", "<leader>t", function()
+	local list = vim.opt.list:get()
+	if list then
+		vim.cmd('set nolist')
+	else
+		vim.cmd('set list')
+	end
+end)
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 
 --Telescope binds--
 local telescope = require('telescope.builtin')
