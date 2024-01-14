200b57e7cde90439e0d53df96674dca7f6eb03e1
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-01-14 14:55:49 -0400

    feat: change config to PC

--- qtile/config.py
+++ qtile/config.py
@@ -7,6 +7,39 @@ from custom_widgets import MicrophoneStatus
 mod = "mod4"
 terminal = guess_terminal()
 
+pc_widgets = [
+    widget.GroupBox(),
+    widget.CheckUpdates(custom_command="yay -S && yay -Qu",
+                        execute='lazy.widget["checkupdates"].force_update()',
+                        no_update_string="󰍕",
+                        update_interval=3600),
+    widget.Prompt(),
+    widget.Spacer(bar.STRETCH),
+    widget.MemoryGraph(type='linefill', line_width=1),
+    widget.CPUGraph(type='linefill', line_width=1,
+                    graph_color='FF8C00',
+                    fill_color='FF8C00'),
+    widget.ThermalZone(zone="/sys/class/hwmon/hwmon1/temp1_input",
+                       update_interval=5,
+                       high=65,
+                       crit=80),
+    widget.Spacer(bar.STRETCH),
+
+    widget.Clock(format="%d/%m %H:%M",
+                 mouse_callbacks={'Button1': lazy.spawn("coretime")}),
+    widget.KeyboardLayout(configured_keyboards=['us', 'br']),
+    widget.TextBox(fmt=' '),
+    widget.PulseVolume(
+        mouse_callbacks={'Button1': lazy.spawn("pavucontrol")},
+        emoji=True,
+        emoji_list=['󰖁', '󰕿', '󰖀', '󰕾'],
+    ),
+    MicrophoneStatus(update_interval=0.5,
+                     mouse_callbacks={'Button1': lazy.spawn("/home/gcorreia/.config/qtile/volume_scripts/mute_mic.sh")}),
+    widget.TextBox(fmt='󰍺',
+                   mouse_callbacks={'Button1': lazy.spawn("arandr")}),
+]
+
 keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
@@ -15,16 +48,12 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
         desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
         desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(),
         desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(),
@@ -32,10 +61,6 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
     Key(
         [mod, "shift"],
         "Return",
@@ -84,14 +109,12 @@ groups = [Group(i) for i in ["dev", "browse", "msg", "personal", "misc", "bg"]]
 for index, i in enumerate(groups):
     keys.extend(
         [
-            # mod1 + letter of group = switch to group
             Key(
                 [mod],
                 str(index + 1),
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
             ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
                 [mod, "shift"],
                 str(index + 1),
@@ -99,10 +122,6 @@ for index, i in enumerate(groups):
                 desc="Switch to & move focused window to group {}".format(
                     i.name),
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
 
@@ -111,90 +130,21 @@ layouts = [
                    border_width=2,
                    border_focus='55AAAA'),
     layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
 ]
 
-widget_defaults = dict(
-    font="sans",
-    fontsize=14,
-    padding=3,
-)
+widget_defaults = dict(font="sans", fontsize=14, padding=3,)
 extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
-            [
-                widget.GroupBox(),
-                widget.CheckUpdates(custom_command="yay -S && yay -Qu",
-                                    execute='lazy.widget["checkupdates"].force_update()',
-                                    no_update_string='<span size="14pt">󰍕</span>',
-                                    update_interval=3600),
-                widget.Prompt(),
-                widget.Spacer(bar.STRETCH),
-                widget.MemoryGraph(type='linefill', line_width=1),
-                widget.CPUGraph(type='linefill', line_width=1,
-                                graph_color='FF8C00',
-                                fill_color='FF8C00'),
-                widget.ThermalZone(zone='/sys/class/thermal/thermal_zone5/temp',
-                                   update_interval=5),
-                widget.Spacer(bar.STRETCH),
-
-                widget.Clock(format="%d/%m %H:%M",
-                             mouse_callbacks={'Button1': lazy.spawn("coretime")}),
-                widget.KeyboardLayout(
-                    fontsize=13, configured_keyboards=['us', 'br']),
-                widget.TextBox(fmt=' '),
-                widget.TextBox(fmt='',
-                               fontsize=14,
-                               padding=3,
-                               mouse_callbacks={'Button1': lazy.spawn("blueman-manager")}),
-                widget.Wlan(interface='wlp0s20f3',
-                            format='󱚽',
-                            disconnected_message='󰖪',
-                            fontsize=20,
-                            padding=3,
-                            mouse_callbacks={'Button1': lazy.spawn("alacritty -e nmtui")}),
-                widget.Volume(emoji="true",
-                              emoji_list=['󰖁', '󰕿', '󰖀', '󰕾'],
-                              fontsize=22,
-                              mute_command=lazy.spawn('pavucontrol'),
-                              padding=3,
-                              mouse_callbacks={'Button1': lazy.spawn("pavucontrol")}),
-                MicrophoneStatus(update_interval=0.5,
-                                 mouse_callbacks={'Button1': lazy.spawn("pavucontrol")}),
-                widget.TextBox(fmt='󰍺',
-                               fontsize=16,
-                               padding=3,
-                               mouse_callbacks={'Button1': lazy.spawn("arandr")}),
-                widget.Battery(format='<span size="14pt">{char}</span><span size="10pt">{percent:2.0%}</span>',
-                               charge_char='󰂄',
-                               discharge_char='󱟤',
-                               padding=3,
-                               fontsize=22),
-            ],
-            24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-        ),
-        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
-        # By default we handle these events delayed to already improve performance, however your system might still be struggling
-        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
-        # x11_drag_polling_rate = 60,
+        bottom=bar.Bar(pc_widgets,
+                       24,
+                       # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+                       # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+                       ),
     ),
 ]
 
-# Drag floating layouts.
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
@@ -211,7 +161,6 @@ floats_kept_above = True
 cursor_warp = False
 floating_layout = layout.Floating(
     float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
@@ -224,20 +173,6 @@ floating_layout = layout.Floating(
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
-
-# When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
