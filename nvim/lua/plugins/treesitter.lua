2f3b0380099e0ca952fc31a277daac242f903450
gabrielclcardoso <gabrielclcardoso@gmail.com>
2026-04-29 21:46:35 -0300

    feat: migrate to treesiter cli

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,37 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main", -- CRITICAL: You must be on the main branch for Neovim 0.12
+    build = ":TSUpdate",
+    config = function()
+        -- 1. Use the plugin ONLY to download your parsers
+        require("nvim-treesitter").install({
+            "json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+            "markdown",
+			"markdown_inline",
+			"bash",
+			"lua",
+			"vim",
+			"vimdoc",
+            "dockerfile",
+			"gitignore",
+			"query",
+			"python",
+        })
 
-		-- configure treesitter
-		treesitter.setup({ -- enable syntax highlighting
-			highlight = {
-				enable = true,
-			},
-			-- enable indentation
-			indent = { enable = true },
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"yaml",
-				"html",
-				"css",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"python",
-			},
-		})
-	end,
+        -- 2. Use NATIVE Neovim to actually do the highlighting
+        -- This tells Neovim: "Every time I open a file, turn on native Treesitter"
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("NativeTreesitter", { clear = true }),
+            callback = function()
+                -- pcall prevents errors if you open a filetype you haven't downloaded a parser for yet
+                pcall(vim.treesitter.start)
+            end,
+        })
+    end,
 }
